1aa92eaf2d2251f902c586dc312e82a24c587081
¨[Angoly <[aaraujom@miumg.edu.gt]>
2026-04-27 11:06:32 -0600

    Cambios en el login

--- proyfinal/src/main/java/Vista/frmLogin.java
+++ proyfinal/src/main/java/Vista/frmLogin.java
@@ -202,64 +202,76 @@ public class frmLogin extends javax.swing.JFrame {
                     
                     switch (areaSeleccionada) {
 
-                        case "Bancos":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }                               
-                     
-                        break;
-                        case "Compras":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }   
-                        break;
-                        case "Cuentas Corrientes":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }   
-                        break;
-                        case "Inventarios":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }   
-                        break;
-                        case "Seguridad":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }   
-                        break;
-                        case "Ventas":
-                        try {
-                            MdiSeguridad menu = new MdiSeguridad();
-                            menu.setVisible(true);
-                            this.dispose();
-                        } catch (Exception e) {
-                            System.out.println(e);
-                        }   
-                        break;
-                        default:
-                            break;
-                    }
+    case "Bancos":
+        try {
+            // Por ahora no abre nada, solo un mensaje informativo, crear el mdí Bancos
+            JOptionPane.showMessageDialog(null, 
+                "Módulo de Bancos aún no disponible", 
+                "Información", 
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    case "Compras":
+        try {
+            // JOptionPane o su propio MDI cuando lo tengan
+            JOptionPane.showMessageDialog(null, 
+                "Módulo de Compras aún no disponible", 
+                "Información", 
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    case "Cuentas Corrientes":
+        try {
+            JOptionPane.showMessageDialog(null, 
+                "Módulo de Cuentas Corrientes aún no disponible", 
+                "Información", 
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    case "Inventarios":
+        try {
+            JOptionPane.showMessageDialog(null, 
+                "Módulo de Inventarios aún no disponible", 
+                "Información", 
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    case "Seguridad":
+        try {
+            MdiSeguridad menu = new MdiSeguridad(); // ← Solo aquí abre el MDI
+            menu.setVisible(true);
+            this.dispose();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    case "Ventas":
+        try {
+            JOptionPane.showMessageDialog(null, 
+                "Módulo de Ventas aún no disponible", 
+                "Información", 
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        break;
+
+    default:
+        break;
+}
 
                 } else {
                     JOptionPane.showMessageDialog(this, "ERROR AL ENCONTRAR USUARIO o CONTRASEÑA",                              "ERROR", JOptionPane.ERROR_MESSAGE);
